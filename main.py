03af32630b7b48d740b98749ec92d1e9c5264597
Huseyn <huseyn09007@gmail.com>
2026-06-11 23:47:47 +0100

    Add parse_proxy: convert proxy data to requests format

--- main.py
+++ main.py
@@ -17,3 +17,27 @@ def load_proxies(filename: str) -> list[str]:
     except Exception as e:
         print(f"Error {e}")
     return proxies
+
+def parse_proxy(proxy_data: str) -> dict|None:
+    """Proxy is sorted and formatted"""
+    if not proxy_data:
+        return None
+    
+    data_parts = proxy_data.split(":")
+
+    if len(data_parts) == 2:
+        host = data_parts[0]
+        port = data_parts[1]
+        url = f"http://{host}:{port}"
+        return {"http": url, "https": url}
+    
+    elif len(data_parts) == 4:
+        host = data_parts[0]
+        port = data_parts[1]
+        user_name = data_parts[2]
+        password = data_parts[3]
+        url = f"http://{user_name}:{password}@{host}:{port}"
+        return {"http": url, "https": url}
+    
+    return None
+
